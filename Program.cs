475a4fa15b71e44203d2f16bf4c9c3b87ee34b48
eric f <github@dogstar.us>
2023-08-27 10:14:21 -0700

    Update Program.cs
    finished it

--- Program.cs
+++ Program.cs
@@ -6,30 +6,6 @@ using System.Runtime.Intrinsics;
 
 namespace ScrappyCoco
 {
-    public static class EnumeratorCloner
-    {
-        public static T Clone<T>(T source) where T : class, IEnumerator<T>
-        {
-            var sourceType = source.GetType().UnderlyingSystemType;
-            var sourceTypeConstructor = sourceType.GetConstructor(new Type[] { typeof(Int32) });
-            var newInstance = sourceTypeConstructor.Invoke(new object[] { -2 }) as T;
-
-            var nonPublicFields = source.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
-            var publicFields = source.GetType().GetFields(BindingFlags.Public | BindingFlags.Instance);
-            foreach (var field in nonPublicFields)
-            {
-                var value = field.GetValue(source);
-                field.SetValue(newInstance, value);
-            }
-            foreach (var field in publicFields)
-            {
-                var value = field.GetValue(source);
-                field.SetValue(newInstance, value);
-            }
-            return newInstance;
-        }
-    }
-
     class StartStop : IComparable<StartStop>
     {
         public int Start;
@@ -49,13 +25,75 @@ namespace ScrappyCoco
         }
     }
 
+    class HStrokePlus : IComparable<HStrokePlus>
+    {
+        public int Y;
+        public List<StartStop>? HStrokeList;
+
+        public int CompareTo(HStrokePlus? other)
+        {
+            return this.Y - other.Y;
+        }
+
+        public HStrokePlus( int y, SortedSet<StartStop> strokes )
+        {
+            Y = y;
+            if (strokes != null)
+            {
+                HStrokeList = new List<StartStop>();
+                foreach (StartStop ss in strokes)
+                {
+                    HStrokeList.Add(ss);
+                }
+            }
+        }
+
+        public StartStop? FindStrokeCrossesX(int x)
+        {
+            if( HStrokeList == null )
+            {
+                return null;
+            }
+
+            if( HStrokeList.Count == 0 )
+            {
+                return null;
+            }
+
+            int l = 0;
+            int h = HStrokeList.Count - 1;
+            while (l <= h)
+            {
+                int m = (l + h) / 2;
+                StartStop ss = HStrokeList[m];
+                if (x < ss.Start)
+                {
+                    h = m - 1;
+                    continue;
+                }
+                if( x > ss.Stop )
+                {
+                    l = m + 1;
+                    continue;
+                }
+                return ss;
+            }
+
+            return null;
+        }
+    }
+
     class Compute
     {
         Dictionary<int, SortedSet<StartStop>> VStrokes = new Dictionary<int, SortedSet<StartStop>>();
         Dictionary<int, SortedSet<StartStop>> HStrokes = new Dictionary<int, SortedSet<StartStop>>();
+        SortedSet<HStrokePlus> HStrokesSorted = new SortedSet<HStrokePlus>();
+        List<Point> FoundCrosses = new List<Point>();
 
         public void Go(char[] dirs, int [] lens)
         {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
             int x = 0;
             int y = 0;
 
@@ -68,50 +106,94 @@ namespace ScrappyCoco
                 {
                     case 'U':
                         {
+                            int final = y + len;
                             bool b = VStrokes.TryGetValue(x, out SortedSet<StartStop> vss);
                             if (!b)
                             {
                                 vss = new SortedSet<StartStop>();
                                 VStrokes[x] = vss;
                             }
-                            vss.Add(new StartStop(y, y + len));
-                            y += len;
+                            StartStop ssAdd = new StartStop(y, final);
+                            vss.TryGetValue(ssAdd, out StartStop ssAlready);
+                            if (ssAlready != null)
+                            {
+                                ssAlready.Stop = Math.Max(ssAlready.Stop, final);
+                            }
+                            else
+                            {
+                                bool bAdded = vss.Add(ssAdd);
+                                Debug.Assert( bAdded );
+                            }
+                            y = final;
                             break;
                         }
                     case 'D':
                         {
+                            int final = y - len;
                             bool b = VStrokes.TryGetValue(x, out SortedSet<StartStop> vss);
                             if (!b)
                             {
                                 vss = new SortedSet<StartStop>();
                                 VStrokes[x] = vss;
                             }
-                            vss.Add(new StartStop(y - len, y));
-                            y -= len;
+                            StartStop ssAdd = new StartStop(final, y);
+                            vss.TryGetValue(ssAdd, out StartStop ssAlready);
+                            if (ssAlready != null)
+                            {
+                                ssAlready.Stop = Math.Max(ssAlready.Stop, y);
+                            }
+                            else
+                            {
+                                bool bAdded = vss.Add(ssAdd);
+                                Debug.Assert(bAdded);
+                            }
+                            y = final;
                             break;
                         }
                     case 'L':
                         {
+                            int final = x - len;
                             bool b = HStrokes.TryGetValue(y, out SortedSet<StartStop> hss);
                             if (!b)
                             {
                                 hss = new SortedSet<StartStop>();
                                 HStrokes[y] = hss;
                             }
-                            hss.Add(new StartStop(x - len, x));
-                            x -= len;
+                            StartStop ssAdd = new StartStop(final, x);
+                            hss.TryGetValue(ssAdd, out StartStop ssAlready);
+                            if (ssAlready != null)
+                            {
+                                ssAlready.Stop = Math.Max(ssAlready.Stop, x);
+                            }
+                            else
+                            {
+                                bool bAdded = hss.Add(ssAdd);
+                                Debug.Assert(bAdded);
+                            }
+                            x = final;
                             break;
                         }
                     case 'R':
                         {
+                            int final = x + len;
                             bool b = HStrokes.TryGetValue(y, out SortedSet<StartStop> hss);
                             if (!b)
                             {
                                 hss = new SortedSet<StartStop>();
                                 HStrokes[y] = hss;
                             }
-                            hss.Add(new StartStop(x, x + len));
-                            x += len;
+                            StartStop ssAdd = new StartStop(x, final);
+                            hss.TryGetValue(ssAdd, out StartStop ssAlready);
+                            if (ssAlready != null)
+                            {
+                                ssAlready.Stop = Math.Max(ssAlready.Stop, final);
+                            }
+                            else
+                            {
+                                bool bAdded = hss.Add(ssAdd);
+                                Debug.Assert(bAdded);
+                            }
+                            x = final;
                             break;
                         }
                 }
@@ -146,7 +228,7 @@ namespace ScrappyCoco
                                 v2.Current.Dead = true;
                                 v.Current.Stop = Math.Max(v.Current.Stop, v2.Current.Stop);
                                 toRemove.Add(v2.Current);
-                                Debug.WriteLine("rem from HStrokes @ {0}, from {1} to {2}", kvp.Key, v2.Current.Start, v2.Current.Stop);
+                                // Debug.WriteLine("rem from HStrokes @ {0}, from {1} to {2}", kvp.Key, v2.Current.Start, v2.Current.Stop);
                             }
                         }
                         else
@@ -192,7 +274,7 @@ namespace ScrappyCoco
                                 v2.Current.Dead = true;
                                 v.Current.Stop = Math.Max(v.Current.Stop, v2.Current.Stop);
                                 toRemove.Add(v2.Current);
-                                Debug.WriteLine("rem from VStrokes @ {0}, from {1} to {2}", kvp.Key, v2.Current.Start, v2.Current.Stop);
+                                // Debug.WriteLine("rem from VStrokes @ {0}, from {1} to {2}", kvp.Key, v2.Current.Start, v2.Current.Stop);
                             }
                         }
                         else
@@ -207,6 +289,10 @@ namespace ScrappyCoco
                 }
             }
 
+            foreach( KeyValuePair<int, SortedSet<StartStop>> kvp in HStrokes)
+            {
+                HStrokesSorted.Add(new HStrokePlus(kvp.Key, kvp.Value));
+            }
             
             // now go through each VStroke and find any matching HStrokes
             foreach (KeyValuePair<int, SortedSet<StartStop>> kvp in VStrokes)
@@ -217,19 +303,31 @@ namespace ScrappyCoco
                 {
                     int startY = vss.Start;
                     int stopY = vss.Stop;
+
                     // find any hstrokes that are within the range of startY to stopY. How do we do this?
                     // how do we find the first hstroke at startY?
+                    SortedSet<HStrokePlus> hss = HStrokesSorted.GetViewBetween(new HStrokePlus(startY, null), new HStrokePlus(stopY, null));
+
+                    // need a quick way to find if any of the strokes in hss.HStrokes cross our xx
+                    // this seems to imply a specialized binary search
+
+                    foreach( HStrokePlus hsp in hss )
+                    {
+                        // quickly find a stroke, if any, that crosses X
+                        StartStop? ssFound = hsp.FindStrokeCrossesX(xx);
+                        if (ssFound != null)
+                        {
+                            FoundCrosses.Add(new Point(xx, hsp.Y));
+                        }
+                    }
                     
                 }
 
             }
 
-            List<int> ll;
-            SortedList<int, int> lll;
-            
-            
-            // now none of the strokes should overlap.
-            int stop = 0;
+            stopwatch.Stop();
+            long seconds = stopwatch.ElapsedMilliseconds / 1000;
+            Console.WriteLine("For {2}, took {0} seconds, found {1} crosses", seconds, FoundCrosses.Count, dirs.Length);
         }
 
         bool StrokesCross(int x, int startY, int stopY, int y, int startX, int stopX)
@@ -246,18 +344,23 @@ namespace ScrappyCoco
         static void Main(string[] args)
         {
             Random r = new Random(1);
-            int n = 1000;
-            char[] cAry = new char[n];
-            int[] nLen = new int[n];
             char[] nDirecs = { 'U', 'D', 'L', 'R' };
-            for (int i = 0; i < n; i++)
+
+            for (int count = 1024; count < 1000000000; count *= 2)
             {
-                int rn = r.Next(0, 4);
-                cAry[i] = nDirecs[rn];
-                nLen[i] = r.Next(1, 100);
+                int n = count;
+                char[] cAry = new char[n];
+                int[] nLen = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    int rn = r.Next(0, 4);
+                    cAry[i] = nDirecs[rn];
+                    nLen[i] = r.Next(1, 100);
+                }
+
+                Compute c = new Compute();
+                c.Go(cAry, nLen);
             }
-            Compute c = new Compute();
-            c.Go(cAry, nLen);
         }
     }
 }
